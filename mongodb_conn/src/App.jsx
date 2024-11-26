f30a35f2f92da0f18d738783d808fb010662a2d9
AbhayGJoshi <joshiag.19@gmail.com>
2024-11-26 10:23:58 +0530

    new 261124

--- mongodb_conn/src/App.jsx
+++ mongodb_conn/src/App.jsx
@@ -1,70 +1,77 @@
-// Frontend code 
+// Frontend code
 // Filename - App.js
 // Filename - App.js
 //video ur - https://www.youtube.com/watch?v=WJNSNFJOaWU
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import "./App.css";
 
-
 function App() {
-    const [name, setName] = useState("");
-    const [userList, setUserList] = useState();
-    const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-    useEffect(()=>{
-        getUsers();
-    },[(userList)]);
+  const [userList, setUserList] = useState();
 
-    
-    let form={name:"", email:""};
-    const handleOnSubmit=async(e)=>{
-        e.preventDefault();
-       const response=await fetch('http://localhost:8080/user',{
-            method: 'POST',
-            body: JSON.stringify( form ),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-        const data= await response.json();
-        // console.log(data);    
-        setUserList(...userList, data) ;           
-    }    
+  useEffect(() => {
+    getUsers();
+  }, []);
 
-    const getUsers=async()=>{
-       const response=await fetch('http://localhost:8080/user',{
-            method: 'GET',
-        });
-        const data= await response.json();
-        console.log(data);    
-        setUserList(...userList, data) ; 
-   }    
+  let form = [{ name }, { email }];
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("http://localhost:8080/user", {
+      method: "POST",
+      body: JSON.stringify(form),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    // console.log(data);
+    // setUserList(...userList, data);
+    const data = await response.json();
+    setUserList(data);
+  };
 
-return (
+  const getUsers = async () => {
+    const response = await fetch("http://localhost:8080/user", {
+      method: "GET",
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+
+  return (
     <div className="formContainer">
-
-        <form action="" className='formCss'>
+      <form action="" className="formCss">
         <h3>This is React WebApp: MongoDB </h3>
 
-            <input type="text" placeholder="enter your name"  className="inputCss"
-            value={name} onChange={(e) => setName(e.target.value)} />
-            <input type="email" placeholder="enter your email" className="inputCss"
-            value={email} onChange={(e) => setEmail(e.target.value)} />
-            <button type="submit" className="buttonCss"
-            onClick={handleOnSubmit}>Submit</button>
-        </form>
-        <div>
+        <input
+          type="text"
+          placeholder="enter your name"
+          className="inputCss"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="email"
+          placeholder="enter your email"
+          className="inputCss"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button type="submit" className="buttonCss" onClick={handleOnSubmit}>
+          Submit
+        </button>
+      </form>
+      <div>
         <ul>
-            <li>UserName---UserEmail</li>
-        {userList && userList.map(user=><li key={user._id}>{user.UserEmail},{user.UserName} </li>)}
+          <li>UserName---UserEmail</li>
+          <li></li>
         </ul>
-        </div>
-
-        </div>
-);
+      </div>
+    </div>
+  );
 }
 export default App;
-
 
 // axios({
 //   // Endpoint to send files
